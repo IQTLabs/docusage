d54db41480b86a8d7d4ca67fd4aa0184d1732957
JJ Ben-Joseph <jj@tensorspace.ai>
2023-05-31 15:02:58 -0400

    Sketch out command line.

--- docusage/__main__.py
+++ docusage/__main__.py
@@ -1,2 +1,22 @@
+import argparse
+
+
 def main():
-    print("Hello World!")
+    parser = argparse.ArgumentParser(
+        description="Analyze documents using Large Language Models."
+    )
+    parser.add_argument(
+        "files", metavar="F", type=str, nargs="+", help="a list of files to be analyzed"
+    )
+    parser.add_argument(
+        "--mission", "-m", type=str, required=False, help="an optional mission prompt"
+    )
+
+    args = parser.parse_args()
+
+    files = args.files
+    prompt = args.prompt
+
+    print("Files:", files)
+    if prompt:
+        print("Prompt:", prompt)
