d163e7f93a3a303a86eb49055cec74ac9efef72f
JJ Ben-Joseph <jj@tensorspace.ai>
2023-06-01 13:16:07 -0400

    Check for OpenAI API key.

--- docusage/__main__.py
+++ docusage/__main__.py
@@ -1,7 +1,13 @@
+import os
 import argparse
 
 
 def main():
+    if not os.environ.get("OPENAI_API_KEY"):
+        raise ValueError(
+            "The environment variable OPENAI_API_KEY is not set. You must have a paid OpenAI account to use DocuSage."
+        )
+
     parser = argparse.ArgumentParser(
         description="Analyze documents using Large Language Models."
     )
