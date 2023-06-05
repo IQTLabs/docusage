680eee3a2968b0120965062f8f7dbdd59046758d
JJ Ben-Joseph <jj@tensorspace.ai>
2023-06-04 23:42:32 -0400

    Correctly display files when drag and dropped.

--- docusage/static/app.js
+++ docusage/static/app.js
@@ -1,6 +1,9 @@
 const dropArea = document.getElementById('dragDropArea');
 const fileInput = document.getElementById('fileInput');
 
+// Array to hold the files
+let filesArray = [];
+
 // Handle drag events
 dropArea.addEventListener('dragover', (e) => {
     e.preventDefault();
@@ -13,7 +16,8 @@ dropArea.addEventListener('dragleave', (e) => {
 
 dropArea.addEventListener('drop', (e) => {
     e.preventDefault();
-    fileInput.files = e.dataTransfer.files;
+    Array.from(e.dataTransfer.files).forEach(file => filesArray.push(file));
+    addFiles(filesArray);
     dropArea.style.border = '2px dashed #ccc';
 });
 
@@ -24,18 +28,24 @@ dropArea.addEventListener('click', (e) => {
 
 // Handle file input change event
 fileInput.addEventListener('change', (e) => {
+    Array.from(e.target.files).forEach(file => filesArray.push(file));
+    addFiles(filesArray);
+});
+
+// Add files to drop area div
+const addFiles = (files) => {
     dropArea.innerHTML = '';
 
-    Array.from(fileInput.files).forEach((file, index) => {
+    files.forEach((file, index) => {
         const fileDiv = document.createElement('div');
         fileDiv.textContent = `📄 ${index + 1}: ${file.name}`;
         dropArea.appendChild(fileDiv);
     });
-});
+};
 
 document.getElementById('createReport').addEventListener('click', async (e) => {
     const formData = new FormData();
-    Array.from(fileInput.files).forEach((file, index) => {
+    filesArray.forEach((file, index) => {
         formData.append("files", file);
     });
     const mission = document.getElementById('missionInput').value;
