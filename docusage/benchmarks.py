d2810dee5a88b3b3412f2046787a1c5af02e7005
JJ Ben-Joseph <jj@tensorspace.ai>
2023-08-28 17:14:15 -0400

    Add more debugging features.

--- docusage/benchmarks.py
+++ docusage/benchmarks.py
@@ -1,10 +1,13 @@
 import asyncio
 from glob import glob
 import os
+from logging import getLogger
 
 from tqdm.asyncio import tqdm
 
 from docusage.analyzer import Mission
+
+logger = getLogger(__name__)
 
 
 def get_document_pairs():
@@ -46,7 +49,9 @@ def check_hallucinations(doc: str, mission: str) -> bool:
     pass
 
 
-async def reference_accuracy_benchmark(max_report_pairs=2):
+async def reference_accuracy_benchmark(
+    max_report_pairs: int = 2, report_path: str = None
+):
     pbar = tqdm(
         total=max_report_pairs * 2,
         desc="Generating and testing reports",
@@ -55,24 +60,42 @@ async def reference_accuracy_benchmark(max_report_pairs=2):
     metrics = {"Artificial intelligence": [0, 0], "Biosecurity": [0, 0]}
 
     for i, (biosecurity_docs, ai_docs) in enumerate(get_document_pairs()):
-        if i > max_report_pairs:
+        if i >= max_report_pairs:
             break
         for mission_type in ["Artificial intelligence", "Biosecurity"]:
-            mission = await Mission.create(
-                biosecurity_docs + ai_docs,
-                mission_type,
-                use_dynamic_section_headers=True,
-            )
+            pbar.update(1)
+            try:
+                mission = await Mission.create(
+                    biosecurity_docs + ai_docs,
+                    mission_type,
+                    use_dynamic_section_headers=True,
+                    ignore_failures=True,
+                )
+            except ValueError as e:
+                logger.error(f"{i}_{mission_type}.txt: could not be generated: {e}")
+                continue
             report = await mission.write_report(inline_refs=True, length="tiny")
+            if report_path:
+                if not os.path.exists(report_path):
+                    os.makedirs(report_path)
+                with open(
+                    os.path.join(report_path, f"{i}_{mission_type}.txt"), "w"
+                ) as f:
+                    f.write(report)
             val = (
                 int(check_if_ai(report))
                 if mission_type == "Artificial intelligence"
                 else check_if_biosecurity(report)
             )
+            if val == 1:
+                logger.info(f"{i}_{mission_type}.txt: is {mission_type}")
+            elif val == 0:
+                logger.info(f"{i}_{mission_type}.txt: is not {mission_type}")
+
             if check_if_ai(report) and check_if_biosecurity(report):
                 val = 0
+                logger.info(f"{i}_{mission_type}.txt: is both AI and Biosecurity")
             metrics[mission_type][val] += 1
-            pbar.update(1)
     pbar.close()
     return metrics
 
